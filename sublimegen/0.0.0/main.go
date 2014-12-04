9537433d6fbd6d07100d41786634300fc47b2c0c
AdeebNqo <adeebnqo@gmail.com>
2014-12-04 13:55:25 +0200

    added gocc. unsure if this is how it is used

--- sublimegen/0.0.0/main.go
+++ sublimegen/0.0.0/main.go
@@ -15,16 +15,38 @@ import (
 	"flag"
 	"fmt"
 	"github.com/nu7hatch/gouuid"
+	"code.google.com/p/gocc/frontend/scanner"
+	"code.google.com/p/gocc/frontend/token"
+	"code.google.com/p/gocc/frontend/parser"
 	"os"
+	"io/ioutil"
 )
 
 var name = flag.String("name", "default", "This is the name of the syntax.")
-var scope = flag.String("scope", "source.default", "This is the scope of the syntax file")
+var scope = flag.String("scope", "source.default", "This is the scope of the syntax file.")
 var fileTypes = flag.String("fileTypes", "default", "Comma seperated list of file types.")
+var source = flag.String("source","defaultinput", "the bnf file for the language you want to highlight.")
 
 func main() {
+	//zola
 	flag.Parse()
 
+	scanner := &scanner.Scanner{}
+	srcBuffer, err := ioutil.ReadFile(*source)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	scanner.Init(srcBuffer, token.FRONTENDTokens)
+	parser := parser.NewParser(parser.ActionTable, parser.GotoTable, parser.ProductionsTable, token.FRONTENDTokens)
+	grammar, err := parser.Parse(scanner)
+	if err != nil {
+		fmt.Printf("Parse error: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(grammar)
+
+	//zola
 	jsonhighlight := `
 
 		{ "name": "%v",
