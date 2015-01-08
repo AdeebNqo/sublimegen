44252e4f58f71536d1fe8b2472bb59067637fb3e
Zola Mahlaza <adeebnqo@gmail.com>
2015-01-07 23:24:19 -0800

    removed escaping of illegal chars, that will be handles by go.

--- sublimegen/convertor.py
+++ sublimegen/convertor.py
@@ -4,13 +4,6 @@ import sys
 import re
 
 
-invalid_escape = re.compile(r'\\[0-7]{1,3}')  # up to 3 digits for byte values up to FF
-
-def replace_with_byte(match):
-    return chr(int(match.group(0)[1:], 8))
-
-def repair(brokenjson):
-    return invalid_escape.sub(replace_with_byte, brokenjson)
 
 #The above methods and variables were written by Martijn Pieters(http://stackoverflow.com/a/15198886/1984350)
 
@@ -19,5 +12,5 @@ outputf = sys.argv[2]
 
 with open (inputf, "r") as myfile:
     data=myfile.read()
-    jsondata = json.loads(repair(data))
+    jsondata = json.loads(data)
     plistlib.writePlist(jsondata, outputf)
