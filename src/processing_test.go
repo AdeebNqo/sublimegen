1e25281b2d739c72fa501b67da6b271d0601fa2c
Adeeb Nqo <adeebnqo@gmail.com>
2015-03-11 16:12:08 +0200

    Added unit tests
    Added unit tests for a few methods.
    Examine the commit if you're interested
    in knowing which methods.

--- src/processing_test.go
+++ src/processing_test.go
@@ -22,6 +22,40 @@ func TestDoesDirExist(t *testing.T){
     t.Error(err);
   }
   if (!doesDirExist(&tempdir)){
-    t.Errorf("doesDirExist does not detect folder that exists.");
+    t.Errorf("doesDirExist() does not detect folder that exists.");
+  }
+}
+
+func TestDeleteDir(t *testing.T){
+  tempdir,err := ioutil.TempDir(".","")
+  if (err!=nil){
+    t.Error(err)
+  }
+  if (deleteDir(&tempdir)!=nil){
+    t.Errorf("deleteDir() does not work. Cannot delete file.");
+  }
+}
+func TestConvertJSONtoPlist(t *testing.T){
+  //creating tmp json file named: test.tmLanguage.json
+  jsontext := []byte("{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"}")
+  err := ioutil.WriteFile("test.tmLanguage.json", jsontext, 0775)
+  if (err!=nil){
+    t.Error(err)
+  }
+  filename := "test"
+  err = convertJSONtoPlist(&filename)
+  if (err!=nil){
+    t.Error(err)
+  }
+}
+
+func TestStartandendwithrb(t *testing.T){
+  correctString := "(hello)"
+  incorrectString := "hello"
+  if (!startandendwithrb(correctString)){
+    t.Errorf("startandendwithrb() does not work. Doesn't detect strings that start with round braces");
+  }
+  if (startandendwithrb(incorrectString)){
+    t.Errorf("startandendwithrb() does not work. Says strings that do not start with round braces do.");
   }
 }
